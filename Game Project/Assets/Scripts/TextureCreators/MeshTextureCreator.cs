0d2e485fe7c4b3f7bbe1354b61ec16413c8ee389
StonebayKyle <stonebay.kyle@gmail.com>
2020-07-25 13:25:24 -0600

    Starts on title screen, making a canvas panel with a noise texture.

---
+++ Game Project/Assets/Scripts/TextureCreators/MeshTextureCreator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class MeshTextureCreator : TextureCreator
+{
+    public override void AttachTextureToComponents()
+    {
+        GetComponent<MeshRenderer>().material.mainTexture = texture;
+    }
+}
---
+++ Game Project/Assets/Scripts/TextureCreators/RawImageTextureCreator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RawImageTextureCreator : TextureCreator
+{
+    public override void AttachTextureToComponents()
+    {
+        GetComponent<RawImage>().material.mainTexture = texture;
+    }
+}
